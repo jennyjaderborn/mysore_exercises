da439ede06a1a8a9c6b280b9b7202243341d94b9
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-02-26 15:00:07 +0100

    exercise 2

---
+++ 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+make_heading('<h1>', 'Hej');
+
+function make_heading($heading, $text) {
+    echo "$heading $text $heading";
+}
+
+
+?>
+
+    
+</body>
+</html>
